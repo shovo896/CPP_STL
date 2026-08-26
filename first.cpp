dea9904a1867b545a4f080abd1703e1725ae09b4
Shuvo <177408979+shovo896@users.noreply.github.com>
2026-08-26 21:20:20 +0600

    fyc

--- first.cpp
+++ first.cpp
@@ -1,19 +1,16 @@
-#include<bits/stdc++.h>
-using namespace std; 
-int main(){
-    int name_of_array[100]; 
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main() {
+    // Initialize array to avoid unused variable warning
+    int name_of_array[100] = {0};
+
     vector<int> v;
+    v.push_back(10); // add element to vector
 
-    v.push_back(10);// vector er moddhe rakhte 
-
-    cout<<"value of index 0: "<<v[0]<<endl; // vector er moddhe theke ber korte 
-    cout<<"size of vector: "<<v.size()<<endl; // vector er size ber korte 
-
-
-
-
-
-
+    cout << "value of index 0: " << v[0] << endl;
+    cout << "size of vector: " << v.size() << endl;
 
     return 0;
 }
