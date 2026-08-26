cda9dcdacc57eb15c72c5faddd38e52f51b19516
Shuvo <177408979+shovo896@users.noreply.github.com>
2026-08-26 21:31:03 +0600

    hbjh

--- first.cpp
+++ first.cpp
@@ -2,16 +2,23 @@
 #include <vector>
 using namespace std;
 
+/*
+vec.push_back() O(1)
+vec.size() O(1)
+*/
+
 int main() {
     // Initialize array and demonstrate usage
-    int name_of_array[100];
-    for(int i = 0; i < 100; ++i) name_of_array[i] = i;
-    cout << "first element of array: " << name_of_array[0] << endl;
-    vector<int> v;
-    v.push_back(10); // add element to vector
+    vector<int> numbers ; 
+    while(1){
+        int x ; 
+        cin >> x ; 
+        if(x == -1) break ;
+        numbers.push_back(x) ;
 
-    cout << "value of index 0: " << v[0] << endl;
-    cout << "size of vector: " << v.size() << endl;
+        
+    }
+    cout<<numbers.size() << endl ;
 
     return 0;
 }
