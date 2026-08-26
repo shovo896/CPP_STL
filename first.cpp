7ed3c24d87852e771964c46d724448db7d0bc3e4
Shuvo <177408979+shovo896@users.noreply.github.com>
2026-08-26 21:18:25 +0600

    jn

--- first.cpp
+++ first.cpp
@@ -3,7 +3,13 @@ using namespace std;
 int main(){
     int name_of_array[100]; 
     vector<int> v;
-    
+
+    v.push_back(10);// vector er moddhe rakhte 
+
+    cout<<"value of index 0: "<<v[0]<<endl; // vector er moddhe theke ber korte 
+    cout<<"size of vector: "<<v.size()<<endl; // vector er size ber korte 
+
+
 
 
 
