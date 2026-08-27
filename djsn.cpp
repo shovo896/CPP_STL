2633575b173f787a87e881c89fe2e7a000ddb9b9
Shuvo <177408979+shovo896@users.noreply.github.com>
2026-08-27 11:26:40 +0600

    gjmnl

--- djsn.cpp
+++ djsn.cpp
@@ -2,7 +2,7 @@
 #include<vector> 
 using namespace std ; 
 int main(){
-    vector<int> numbers ;
+    vector<int> numbers(5,2) ;
     numbers.push_back(10) ;
     cout<< "size of numbers : " << numbers.size() << endl ; 
     for(int i=0;i<numbers.size();i++){
