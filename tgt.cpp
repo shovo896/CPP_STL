c2536104949a82b6ee5af9d8771b0d883585ccc9
Shuvo <suvo10.info@gmail.com>
2026-09-07 00:02:20 +0600

    thws

---
+++ tgt.cpp
@@ -0,0 +1,11 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+       string a; 
+       cin >> a;
+       cout << a <<'\n';
+       // sort 
+
+       sort(a.begin(), a.end());
+       cout << a <<'\n';
+}
