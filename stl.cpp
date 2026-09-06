dbcd462a9790c5282e2c362f67438efaa949d194
Shuvo <suvo10.info@gmail.com>
2026-09-06 20:16:15 +0600

    sv

---
+++ stl.cpp
@@ -0,0 +1,11 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+       string st; 
+       cin >> st; 
+       cout << st <<'\n';
+
+       // size
+       cout << st.size() <<'\n';
+
+}
