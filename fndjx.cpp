b6464308f6c188a470d03d2b813300f91045ebb5
Shuvo <suvo10.info@gmail.com>
2026-09-07 08:37:21 +0600

    egs

--- fndjx.cpp
+++ fndjx.cpp
@@ -3,4 +3,9 @@ using namespace std;
 int main(){
        string st = "abcd1234";
        cout << st.substr(0, 4) <<'\n';
+       cout << st;
+       
+       st.erase(0, 4);
+       cout << st <<'\n';
+
 }
