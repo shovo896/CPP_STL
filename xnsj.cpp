828a22502bbd7a5a6d967e65e7fe07df3c27bbd7
Shuvo <suvo10.info@gmail.com>
2026-09-07 08:26:05 +0600

    uhj

---
+++ xnsj.cpp
@@ -0,0 +1,12 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+       //string st[5];
+       vector<string> st(5);
+       for (int i = 0; i<5; i++){
+              cin >> st[i];
+       }
+       cout << st[2] << '\n';
+       sort(st.begin(), st.end());
+       cout << st[2] << '\n';
+}
