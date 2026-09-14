245cc5d2f3d243591b0cc738a981bc2699aa4d5f
Shuvo <suvo10.info@gmail.com>
2026-09-14 16:53:45 +0600

    v dxs

---
+++ problem2.cpp
@@ -0,0 +1,10 @@
+class Solution :
+public: 
+bool isDigitSumPlaindrome(int n ){
+       string st = to_string(n);
+       int sum = 0; 
+       for (auto c :st){
+              sum += c - '0';
+       }
+       string a = to.string(sum);
+}
