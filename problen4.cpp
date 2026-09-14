282e19dfdc187df654eb5d7ae2cbb4c503f3bb51
Shuvo <suvo10.info@gmail.com>
2026-09-14 22:47:04 +0600

    dfrs

--- problen4.cpp
+++ problen4.cpp
@@ -3,5 +3,21 @@ using namespace std;
 int main(){
        string s1, s2; 
        cin >> s1 >> s2; 
-       for (auto )
+       for (auto x : s1){
+              x = tolower(x);
+       }
+       for (auto x : s2){
+              x = tolower(x);
+       }
+       cout << s1 << "\n"; 
+       cout << s2 << "\n";
+       if (s1<s2){
+              cout << -1 << endl;
+       }
+       else if (s1>s2){
+              cout << 1 << endl;
+       }
+       else{
+              cout << "0\n" << endl;
+       }
 }
