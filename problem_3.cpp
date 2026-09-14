692a8bbbc7cc3688c268b9dd18ee59231692dec9
Shuvo <suvo10.info@gmail.com>
2026-09-14 18:41:46 +0600

    jcncj

---
+++ problem_3.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+       string s; 
+       cin >> s;
+       string ans; 
+       for (auto  x : s){
+              x = tolower(x);
+              if (x == 'a' || x == 'e' || x == 'i' || x == 'o' || x == 'u'|| x == 'y'){
+                     continue; 
+              }
+              ans += x;
+
+       }
+
+}
