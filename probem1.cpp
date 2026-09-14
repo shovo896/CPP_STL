f81758bef12996646da98c1a643d3d9194180787
Shuvo <suvo10.info@gmail.com>
2026-09-14 15:28:26 +0600

    lp

---
+++ probem1.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+       int n;
+       cin >> n;
+       vector<string> a;
+       for (int i = 0; i < n; i++){
+              string st;
+              cin >> st;
+              a.push_back(st);
+
+       }
+}
