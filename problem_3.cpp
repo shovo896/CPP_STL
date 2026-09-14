ccc3c1252e84a5be1e1a1840402ca144238d1923
Shuvo <suvo10.info@gmail.com>
2026-09-14 18:42:09 +0600

    cjn

--- problem_3.cpp
+++ problem_3.cpp
@@ -9,8 +9,9 @@ int main(){
               if (x == 'a' || x == 'e' || x == 'i' || x == 'o' || x == 'u'|| x == 'y'){
                      continue; 
               }
-              ans += x;
+              ans += "." + x;
 
        }
+       cout << ans << endl;
 
 }
