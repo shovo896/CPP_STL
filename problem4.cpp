72f0821d4deaafd2dd96f85d84a0e6049aba069a
Shuvo <suvo10.info@gmail.com>
2026-09-15 07:33:24 +0600

    jhik

---
+++ problem4.cpp
@@ -0,0 +1,22 @@
+#include<bits/stdc++.h> 
+using namespace std; 
+int cntVowel(string s){
+       int cnt = 0; 
+       for (auto x :s){
+              cnt += (x=='a' || x=='e' || x=='i' || x=='o' || x=='u' || x=='A' || x=='E' || x=='I' || x=='O' || x=='U');
+       }
+       return cnt;
+
+       int main(){
+              string s1, s2, s3;
+              bool ans = true;
+              cin >> s1;
+              if (cntVowel(s1)!=5) ans = false;
+              cin >> s2; 
+              if (cntVowel(s2)!=7) ans = false; 
+              cin >> s3; 
+              if (cntVowel(s3)!=5) ans = false; 
+              if (ans) cout << "YES\n";
+              else cout << "NO\n";
+       }
+}
