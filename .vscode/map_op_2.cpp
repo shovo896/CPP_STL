1db04d915d7ad58b713e3fbd5a3fdeb0a78c553d
Shuvo <suvo10.info@gmail.com>
2026-09-19 18:38:29 +0600

    odikap

---
+++ .vscode/map_op_2.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+#define int long long
+#define ll long long
+
+using namespace std;
+
+int main()
+{
+       ios_base::sync_with_stdio(0), cin.tie(0), cout.tie(0);
+
+
+
+
+
+
+
+       
+
+       return 0; 
+} 
