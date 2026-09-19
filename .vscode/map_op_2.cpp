c312f356f9ea5eab6cdecff52f96b0cf8bc1b03b
Shuvo <suvo10.info@gmail.com>
2026-09-19 18:41:22 +0600

    irjis

--- .vscode/map_op_2.cpp
+++ .vscode/map_op_2.cpp
@@ -7,14 +7,17 @@ using namespace std;
 int main()
 {
        ios_base::sync_with_stdio(0), cin.tie(0), cout.tie(0);
+       int n = 5; 
+       map<int, string> mp;
+       cin >> n;
+       for (int i = 0; i < n;i++){ 
+              int x ,y ; 
+              cin >> x >> y; 
+              mp[x] = y;
+       }
+       for (auto& it : mp){ 
+              cout << it.first << " " << it.second << "\n";
+       }
 
-
-
-
-
-
-
-       
-
-       return 0; 
+              return 0; 
 } 
