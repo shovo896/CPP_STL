df722be3b38dcc9bd4b2ff728a34fc922b65fd6e
Shuvo <suvo10.info@gmail.com>
2026-09-19 18:42:26 +0600

    ofkbhposf

--- .vscode/map_op_2.cpp
+++ .vscode/map_op_2.cpp
@@ -1,23 +1,28 @@
 #include <bits/stdc++.h>
-#define int long long
-#define ll long long
-
 using namespace std;
 
 int main()
 {
-       ios_base::sync_with_stdio(0), cin.tie(0), cout.tie(0);
-       int n = 5; 
-       map<int, string> mp;
+       ios::sync_with_stdio(false);
+       cin.tie(nullptr);
+
+       int n;
        cin >> n;
-       for (int i = 0; i < n;i++){ 
-              int x ,y ; 
-              cin >> x >> y; 
+
+       map<int, string> mp;
+
+       for (int i = 0; i < n; i++)
+       {
+              int x;
+              string y;
+              cin >> x >> y;
               mp[x] = y;
        }
-       for (auto& it : mp){ 
-              cout << it.first << " " << it.second << "\n";
+
+       for (const auto &it : mp)
+       {
+              cout << it.first << ' ' << it.second << '\n';
        }
 
-              return 0; 
-} 
+       return 0;
+}
