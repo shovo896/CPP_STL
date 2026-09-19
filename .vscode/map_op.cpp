d933c6e8c526d5e6d790ae45b1150db9964b5e1f
Shuvo <suvo10.info@gmail.com>
2026-09-19 16:56:33 +0600

    kfcmxl

---
+++ .vscode/map_op.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+#define int long long
+#define ll long long
+
+using namespace std;
+int main(){
+       ios_base::sync_with_stdio(0), cin.tie(0), cout.tie(0);
+       map <int ,int> mp;
+       mp[3] = 7;
+       mp[1] = 8;
+       mp[10] = 100; 
+       cout << mp[3] << " " << mp[1] << " " << mp[10] << "\n";
+
+       /// access and edit
+       mp[10] = 90;
+       cout << mp[10] << "\n";
+
+       //cout << mp[0] << "\n"; // 0
+}
